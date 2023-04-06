fa4908a60a3764e57e1c6ff8cf036d5ac627da63
Axi <98420477+axiand@users.noreply.github.com>
2023-04-06 13:24:57 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# user-simulator
+Configurable Discord chatbot that learns from users
+
+# Features
+
+- talks to users
+
+- permanently saves things it learned to a database
+
+- can have a rough understanding of what you're talking about
+
+# Note
+It's a good idea to take a look at the usersim.js file first and change the configuration if you want to. The only thing you *have* to do is set the TOKEN variable to whatever the token of your bot is.
+
+# How do I get the token?
+https://discord.com/developers/applications and create an application with a bot user
+
+# Installing
+
+- npm install
+
+- make a script that starts the bot
+
+- if everything goes right, you'll have your own chatbot
+
+# Config
+
+`LEARNS_FROM` - array of channel names the bot will learn messages from
+
+`TALKS_IN` - array of channel names the bot talks in
+
+`TOKEN` - pretty obvious
+
+# Contributing
+
+All contributions that make the bot overall better or fix things are welcome.
---
+++ usersim.js
@@ -0,0 +1,75 @@
+//configuration:
+const LEARNS_FROM = ["the-chamber"] //channels the bot learns from but doesnt talk in
+const TALKS_IN = ["the-chamber"] //channels the bot will talk to people in
+const TOKEN = '...' //the token of your bot
+//end configuration
+
+const discord = require("discord.js")
+const bot = new discord.Client({disableEveryone: true})
+const fs = require("fs")
+
+if(!fs.readdirSync("./").includes("db.json")) {
+	fs.writeFileSync("./db.json", "{}")
+	console.log("Database does not exist, so a new file was created")
+}
+let db = require("./db.json") //get the database (or make a new file if it does not exist)
+keys = Object.keys(db) //turn the db into an array to perform array operations on it
+
+function think(filter, strict) {
+	if(filter) {
+		if(filter.includes("@")) return "nice try sucker" //no ping
+		filteredkeys = keys.filter(k => k.includes(filter)) //filter db keys
+		if(filteredkeys.length == 0 && strict == true) return `No matches for ${filter}` //return if no matches and in strict mode
+		if(filteredkeys.length < 2 && strict == false) filteredkeys = keys //reset keys if no matches and not in strict mode
+		
+		return filteredkeys[Math.round(filteredkeys.length*Math.random())]||filteredkeys[0] //return whatever the bot thought of
+	}
+	return filteredkeys[Math.round(filteredkeys.length*Math.random())]||filteredkeys[0]
+}
+
+bot.on("ready", async () => {
+	console.log("Ready")
+	let link = await bot.generateInvite(["SEND_MESSAGES"])
+	console.log(link)
+	
+	console.log(`[${bot.guilds.cache.size}] ${bot.guilds.cache.map(g => g.name).join(", ")}`)
+}) //do all of this when ready
+
+bot.on("message", async message => {
+	if(message.author.bot) return;
+	
+	if(!LEARNS_FROM.includes(message.channel.name) & message.channel.type !== "dm") return;
+	if(message.content.startsWith(".")) return;
+	if(message.content.includes("@")) return; //avoiding mass ping disasters
+	
+	console.log(`[${message.guild.name}] ${message.author.tag} > ${message.cleanContent}`)
+	
+	if(!keys.includes(message.cleanContent) && message.content.length > 0 && LEARNS_FROM.includes(message.channel.name) && !message.content.includes("discord.gg")) {
+		db[message.cleanContent] = {
+			tag: Math.random()
+		} //make a db entry
+			
+		await fs.writeFile("./db.json", JSON.stringify(db), err => {
+			if(err) throw err;
+		}) //ctrl+s
+			
+		keys = Object.keys(db)
+	} //if the message isnt in the database and is in a channel that the bot can learn from, add it
+	
+	if(!TALKS_IN.includes(message.channel.name)) return;
+	
+	x = 0
+	while(x < message.content.split(" ").length-1 && Math.random() < 0.72) x++ //choose a word, words closer to the end of the message are less likely to be picked
+	
+	m = think(message.content.split(" ")[x], false) //run think() with the chosen word (x)
+	
+	try {
+		await message.channel.send(m) //send le message
+		console.log(`[${message.guild.name}] > ${m}`)
+	} catch(e) {
+		message.reply("Something went wrong, try again") //if something goes horribly wrong
+	}
+})
+
+if(!TOKEN || typeof TOKEN !== "string") return console.log("Token is invalid! Check the TOKEN variable in the bot code and type 'rs' when done.")
+bot.login(TOKEN); //log in with the token
